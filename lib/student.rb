e0e71470c1cd996742100e6092602063da4e7abf
Dean L Betty <deanlloyd64@gmail.com>
2019-10-12 02:00:19 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -53,8 +53,7 @@ class Student
   
   def self.new_from_db(row)
     new_student = self.new(row[1], row[2])
-    new_student.id = row[0]
-    new_student
+    new_student.save
   end
   
   def self.find_by_name(name)
